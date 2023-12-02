ad8c62afceece008b82915001c0af50b86faf0a3
unknown <wolfyrony12@gmail.com>
2023-12-01 21:51:45 -0300

    atividade de conexão com banco

---
+++ components/Firebase.js
@@ -0,0 +1,14 @@
+import {initializeApp} from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCO4nwOI5933OKcM6qre3hZ8HWxnu5U1Hk",
+  authDomain: "acessobdloja-46e34.firebaseapp.com",
+  projectId: "acessobdloja-46e34",
+  storageBucket: "acessobdloja-46e34.appspot.com",
+  messagingSenderId: "383496013260",
+  appId: "1:383496013260:web:a83676126fb422e8dc4969"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
